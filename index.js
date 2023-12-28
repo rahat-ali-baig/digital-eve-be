71016ba285e7914fee4255fea0bd471ba91cd244
Rahat <111171388+rahatmughal@users.noreply.github.com>
2023-12-28 11:19:02 +0500

    :fire: Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 require('dotenv').config();
 const PORT = process.env.PORT || 7000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen();
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
